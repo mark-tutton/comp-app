bd2d706c4cf4e88ce3e4caa37377c7b8567054b5
job_talk <job-talk@users.noreply.github.com>
2020-01-12 16:15:26 -0600

    env

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const mongodb = require('mongodb')
 
-const connectionString = 'mongodb+srv://complexApp:VLlrdRPAmwpJvxSo@cluster0-pk0kc.mongodb.net/ComplexApp?retryWrites=true&w=majority'
-
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     module.exports = client.db()
     const app = require('./app')
-    app.listen(3000)
+    app.listen(process.env.PORT)
 })
